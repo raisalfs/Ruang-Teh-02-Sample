bb510b55ccd493c82f3647327c8025834fb71589
Rais AlFani Lubis <56205371+raisalfs@users.noreply.github.com>
2021-05-07 02:04:28 +0700

    Delete CustomMultiSearchView directory

--- CustomMultiSearchView/app/src/main/java/com/rafslab/materialsearchview/spring/RecyclerView/SpringyAdapterAnimationType.java
+++
@@ -1,8 +0,0 @@
-package com.rafslab.materialsearchview.spring.RecyclerView;
-
-public enum SpringyAdapterAnimationType {
-    SLIDE_FROM_BOTTOM,
-    SLIDE_FROM_RIGHT,
-    SLIDE_FROM_LEFT,
-    SCALE
-}
